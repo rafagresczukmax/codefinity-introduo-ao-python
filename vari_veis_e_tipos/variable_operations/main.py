80ff3a8b8beb536775fc7e30932d7aaec11e965d
rafagresczukmax <>
2026-02-03 22:12:25 +0000

    Variable operations

---
+++ vari_veis_e_tipos/variable_operations/main.py
@@ -0,0 +1,7 @@
+item_name = "Soda"
+item_price = 6.99
+purchase_quantity = 5
+total_cost = purchase_quantity*item_price
+
+# Testing
+print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
